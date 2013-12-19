7e1d1b2578d5991d07b0dc51a0c709ec7978875d
Denny <dengjun86@gmail.com>
2013-12-20 07:16:47 +0800

    go on share page

--- src/com/facsu/publicartmap/explore/ShareArtworkActivity.java
+++ src/com/facsu/publicartmap/explore/ShareArtworkActivity.java
@@ -1,6 +1,8 @@
 package com.facsu.publicartmap.explore;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.facsu.publicartmap.R;
@@ -14,6 +16,14 @@ public class ShareArtworkActivity extends PMActivity {
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_share);
+		setTitle(getString(R.string.title_share));
+		setRightButton(R.drawable.title_send, new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 		
 		addressTv = (TextView) findViewById(R.id.share_location);
 		addressTv.setText(getIntent().getStringExtra("address") );
