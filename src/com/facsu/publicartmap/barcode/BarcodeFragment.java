6287f7ffae33c077cc3ccd17614fda4ab28ff78c
Deng Jun <dengjun86@gmail.com>
2013-12-27 17:15:01 +0800

    add zxing lib

--- src/com/facsu/publicartmap/barcode/BarcodeFragment.java
+++ src/com/facsu/publicartmap/barcode/BarcodeFragment.java
@@ -1,26 +1,41 @@
 package com.facsu.publicartmap.barcode;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.facsu.publicartmap.R;
-import com.facsu.publicartmap.app.PMFragment;
+import com.facsu.publicartmap.widget.TitleBar;
+import com.google.zxing.Result;
+import com.welcu.android.zxingfragmentlib.BarCodeScannerFragment;
 
-public class BarcodeFragment extends PMFragment {
+public class BarcodeFragment extends BarCodeScannerFragment {
 	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_barcode, container, false);
-		return v;
+	private TitleBar title;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.setmCallBack(new IResultCallback() {
+			@Override
+			public void result(Result lastResult) {
+				Toast.makeText(getActivity(), "Scan: " + lastResult.toString(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
-		setTitle(R.string.title_barcode);
+	protected int contentResId() {
+		return R.layout.fragment_barcode;
 	}
 	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		title = (TitleBar) view.findViewById(R.id.titlebar);
+		title.setTitle(getString(R.string.title_barcode));
+		title.setLeftButton(0, null);
+	}
+
 }
