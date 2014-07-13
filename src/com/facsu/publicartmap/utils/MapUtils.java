d65e650d2fa82b2edcecdeb1fed06ef0de4e7ce5
Denny <dengjun86@gmail.com>
2014-07-13 08:45:45 +0800

    Add google map v2 support judge

--- src/com/facsu/publicartmap/utils/MapUtils.java
+++ src/com/facsu/publicartmap/utils/MapUtils.java
@@ -1,5 +1,8 @@
 package com.facsu.publicartmap.utils;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 public class MapUtils {
 
 	private static final double EARTH_RADIUS = 6378.137;
@@ -20,6 +23,37 @@ public class MapUtils {
 
 	private static double rad(double d) {
 		return d * Math.PI / 180.0;
+	}
+	
+	/**
+	 * 判断是否存在支持Google Map API v1
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isSupportGoogleMap(Context context) {
+		PackageManager packageManager = context.getPackageManager();
+		boolean hasGoogleMapApi = false;
+		String[] sharedLibraryNames = packageManager
+				.getSystemSharedLibraryNames();
+		if (sharedLibraryNames != null) {
+			for (String sharedLibraryName : sharedLibraryNames) {
+				if ("com.google.android.maps".equals(sharedLibraryName)) {
+					try {
+						@SuppressWarnings("rawtypes")
+						Class cl = Class
+								.forName("com.google.android.maps.MapActivity");
+						if (cl != null) {
+							hasGoogleMapApi = true;
+						}
+					} catch (Throwable e) {
+						hasGoogleMapApi = false;
+					}
+					break;
+				}
+			}
+		}
+		return hasGoogleMapApi;
 	}
 
 }
