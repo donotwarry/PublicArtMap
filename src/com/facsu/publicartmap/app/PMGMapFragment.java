f70b13c0201f9c54459b4dec5047020fa7e17d61
Denny <dengjun86@gmail.com>
2014-07-27 10:44:34 +0800

    explore on gmap

--- src/com/facsu/publicartmap/app/PMGMapFragment.java
+++ src/com/facsu/publicartmap/app/PMGMapFragment.java
@@ -1,14 +1,199 @@
 package com.facsu.publicartmap.app;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.dennytech.common.service.dataservice.mapi.MApiService;
+import com.facsu.publicartmap.R;
+import com.facsu.publicartmap.widget.TitleBar;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.umeng.analytics.MobclickAgent;
 
 public class PMGMapFragment extends SupportMapFragment {
 
-//	@Override
-//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//			Bundle savedInstanceState) {
-//		View view = inflater.inflate(R.layout.fragment_gmap, null);
-//		return view;
-//	}
+	private TitleBar titleBar;
 
+	@Override
+	public void onResume() {
+		super.onResume();
+		MobclickAgent.onPageStart(getClass().getSimpleName());
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		MobclickAgent.onPageEnd(getClass().getSimpleName());
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		titleBar = (TitleBar) view.findViewById(R.id.titlebar);
+	}
+
+	public void setTitle(String title) {
+		if (titleBar != null) {
+			titleBar.setTitle(title.toString());
+		} else {
+			getActivity().setTitle(title);
+		}
+
+	}
+
+	public void setLeftButton(int resId, OnClickListener listener) {
+		titleBar.setLeftButton(resId, listener);
+	}
+
+	public void setRightButton(int resId, OnClickListener listener) {
+		titleBar.setRightButton(resId, listener);
+	}
+
+	public void enableBackButton(boolean enable) {
+		titleBar.enableBackButton(enable);
+	}
+
+	public void setTitle(int resId) {
+		setTitle(getString(resId));
+	}
+	
+	public MApiService mapiService() {
+		return PMApplication.instance().mapiService();
+	}
+	
+	// ////////////
+		// Dialog相关
+		// ////////////
+
+		protected Dialog managedDialog;
+
+		/**
+		 * 显示progress对话框 </br></br> progress对话框应该只在<b>主线程</b>中被显示
+		 * 
+		 * @param message
+		 */
+		final public void showProgressDialog(String message) {
+			dismissDialog();
+			ProgressDialog dlg = new ProgressDialog(getActivity());
+			dlg.setOnCancelListener(new DialogInterface.OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					onProgressDialogCancel();
+				}
+			});
+			dlg.setMessage(message);
+
+			managedDialog = dlg;
+			dlg.show();
+		}
+
+		protected void onProgressDialogCancel() {
+			// TO OVERRIDE
+		}
+
+		final public void showDialog(String title, String message,
+				DialogInterface.OnClickListener listener) {
+			dismissDialog();
+			AlertDialog dlg = createDialog(title, message);
+			dlg.setButton(DialogInterface.BUTTON_POSITIVE, "确定", listener);
+
+			managedDialog = dlg;
+			dlg.show();
+		}
+
+		final public void showDialog(String title, String message,
+				String positiveText,
+				DialogInterface.OnClickListener positiveListener,
+				String negativeText,
+				DialogInterface.OnClickListener negativeListener) {
+			dismissDialog();
+			AlertDialog dlg = createDialog(title, message);
+			dlg.setButton(DialogInterface.BUTTON_POSITIVE, positiveText,
+					positiveListener);
+			dlg.setButton(DialogInterface.BUTTON_NEGATIVE, negativeText,
+					negativeListener);
+
+			managedDialog = dlg;
+			dlg.show();
+		}
+
+		final public AlertDialog createDialog(String title, String message) {
+			AlertDialog dlg = new AlertDialog.Builder(getActivity()).create();
+			dlg.setTitle(title);
+			dlg.setMessage(message);
+
+			managedDialog = dlg;
+			return dlg;
+		}
+
+		final public void dismissDialog() {
+			if (managedDialog != null && managedDialog.isShowing()) {
+				managedDialog.dismiss();
+			}
+			managedDialog = null;
+		}
+	
+	private SharedPreferences sharePref;
+
+	public SharedPreferences preferences() {
+		if (sharePref == null) {
+			sharePref = getActivity().getSharedPreferences(
+					getActivity().getPackageName(), Context.MODE_PRIVATE);
+		}
+		return sharePref;
+	}
+	
+	//
+	// login & register
+	//
+
+	public static final int REQUEST_CODE_LOGIN = 1001;
+
+	public void gotoLogin(String path) {
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("pam://login?"
+				+ (path == null ? "" : path)));
+		startActivityForResult(intent, REQUEST_CODE_LOGIN);
+	}
+
+	public void gotoRegister(String path) {
+		Intent intent = new Intent(Intent.ACTION_VIEW,
+				Uri.parse("pam://register?" + (path == null ? "" : path)));
+		startActivityForResult(intent, REQUEST_CODE_LOGIN);
+	}
+
+	public void onLoginSuccess() {
+		// sub class implement
+	}
+
+	public void onLoginFailed() {
+		// sub class implement
+	}
+
+	public void onLoginCancel() {
+		// sub class implement
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE_LOGIN) {
+			if (resultCode == Activity.RESULT_OK) {
+				onLoginSuccess();
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				onLoginCancel();
+			} else {
+				onLoginFailed();
+			}
+
+		}
+	}
 }
--- src/com/facsu/publicartmap/explore/ExploreGMapFragment.java
+++ src/com/facsu/publicartmap/explore/ExploreGMapFragment.java
@@ -1,7 +1,170 @@
 package com.facsu.publicartmap.explore;
 
-import com.facsu.publicartmap.app.PMGMapFragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class ExploreGMapFragment extends PMGMapFragment {
+import com.baidu.platform.comapi.basestruct.GeoPoint;
+import com.dennytech.common.service.dataservice.mapi.CacheType;
+import com.dennytech.common.service.dataservice.mapi.MApiRequest;
+import com.dennytech.common.service.dataservice.mapi.MApiRequestHandler;
+import com.dennytech.common.service.dataservice.mapi.MApiResponse;
+import com.dennytech.common.service.dataservice.mapi.impl.BasicMApiRequest;
+import com.dennytech.common.util.Log;
+import com.facsu.publicartmap.R;
+import com.facsu.publicartmap.app.PMGMapFragment;
+import com.facsu.publicartmap.bean.Artwork;
+import com.facsu.publicartmap.bean.GetArtworksByGPSResult;
+import com.facsu.publicartmap.bean.Location;
+import com.facsu.publicartmap.bean.User;
+import com.facsu.publicartmap.utils.MapUtils;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class ExploreGMapFragment extends PMGMapFragment implements
+		OnClickListener, MApiRequestHandler {
+
+	private View rootView;
+	private View progress;
+
+	boolean isRequest = false;
+	boolean isFirstLoc = true;
+	boolean isClickOnPop = false;
+
+	private MApiRequest request;
+	private Artwork[] data;
+	private Artwork curArtwork;
+	private GeoPoint curGP;
+	private Location myLoc;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (rootView == null) {
+			rootView = inflater.inflate(R.layout.fragment_explore_gmap, null);
+			rootView.findViewById(R.id.mylocation).setOnClickListener(this);
+		}
+		ViewGroup parent = (ViewGroup) rootView.getParent();
+		if (parent != null) {
+			parent.removeView(rootView);
+		}
+		progress = rootView.findViewById(R.id.progressbar);
+		return rootView;
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		setLeftButton(R.drawable.title_referesh, this);
+		setRightButton(R.drawable.title_add, this);
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		setTitle(R.string.title_explore);
+	}
+
+	@Override
+	public void onDestroy() {
+		if (request != null) {
+			mapiService().abort(request, this, true);
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.title_left_btn) {
+			refereshData();
+
+		} else if (v.getId() == R.id.title_right_btn) {
+			User user = User.read(preferences());
+			if (user == null) {
+				gotoLogin(null);
+			} else {
+				Intent i = new Intent(Intent.ACTION_VIEW,
+						Uri.parse("pam://share"));
+				if (myLoc != null) {
+					i.putExtra("location", myLoc);
+				}
+				startActivity(i);
+			}
+
+		} else if (v.getId() == R.id.mylocation) {
+			if (myLoc != null) {
+				CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(
+						myLoc.latitude, myLoc.longitude));
+
+				getMap().moveCamera(center);
+				Toast.makeText(getActivity(),
+						myLoc.latitude + ", " + myLoc.longitude,
+						Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getActivity(), getString(R.string.msg_locating),
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	// TODO 求半径内
+	private void refereshData() {
+		CameraPosition cp = getMap().getCameraPosition();
+		LatLng target = cp.target;
+		double clat = target.latitude;
+		double clng = target.longitude;
+		double tlat = target.latitude + 0.1;
+		double tlng = target.longitude + 0.1;
+		int disKM = (int) (MapUtils.getDistance(clat, clng, tlat, tlng));
+		loadData(clat, clng, disKM);
+	}
+
+	private void loadData(double lat, double lng, int rangeKM) {
+		progress.setVisibility(View.VISIBLE);
+		if (request != null) {
+			mapiService().abort(request, this, true);
+		}
+		request = BasicMApiRequest.mapiGet(
+				"http://web358082.dnsvhost.com/ACservice/ACService.svc/GetArtworksByGPS/"
+						+ lng + "/" + lat + "/" + rangeKM, CacheType.NORMAL,
+				GetArtworksByGPSResult.class);
+		mapiService().exec(request, this);
+
+		Log.i("explore", "load data [lat:" + lat + " lng:" + lng + " range:"
+				+ rangeKM + "]");
+	}
+
+	@Override
+	public void onRequestFailed(MApiRequest arg0, MApiResponse arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRequestFinish(MApiRequest arg0, MApiResponse arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRequestProgress(MApiRequest arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRequestStart(MApiRequest arg0) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
